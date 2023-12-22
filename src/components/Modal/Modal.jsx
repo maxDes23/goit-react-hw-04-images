36402da56546a8d53814c4599e398fadaae4edbe
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2023-12-23 00:54:29 +0200

    dsz

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,6 @@
 import React, { useEffect } from 'react';
 
 const Modal = ({ largeImageURL, onClose }) => {
-  const handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      onClose();
-    }
-  };
-
   const handleClose = e => {
     if (e.target === e.currentTarget) {
       onClose();
@@ -26,6 +20,7 @@ const Modal = ({ largeImageURL, onClose }) => {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [onClose]);
+
   return (
     <div className="Overlay" onClick={handleClose}>
       <div className="Modal">
